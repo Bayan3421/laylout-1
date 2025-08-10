29024281a46e369c2391097b4a498eb36e0cdb80
Bayan3421 <115390814+Bayan3421@users.noreply.github.com>
2025-08-10 20:25:32 +0500

    second commit swiper

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-var swiper = new Swiper(".promo_swiper", {});
+var swiper = new Swiper(".promo_swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".promo .arrow_next",
+        prevEl: ".promo .arrow_prew",
+      },
+});
